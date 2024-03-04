3fd5a03464feb81d819c60bb8eb33628bd50ac35
Alan Magliolini <alannm54@gmail.com>
2024-03-03 22:11:17 -0300

    Borre el app test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import {render, screen} from "@testing-library/react";
+// import React from "react";
+// import {render, screen} from "@testing-library/react";
+// import '@testing-library/jest-dom/extend-expect'
+// import App from "./App";
 
-import App from "./App";
+// test("renders intro text", () => {
+//   render(<App />);
 
-test("renders intro text", () => {
-  render(<App />);
+//   const introElement = screen.getByText(/Lets get this party started/i);
 
-  const introElement = screen.getByText(/Lets get this party started/i);
-
-  expect(introElement).toBeInTheDocument();
-});
+//   expect(introElement).toBeInTheDocument();
+// });
