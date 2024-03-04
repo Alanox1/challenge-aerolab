c67faffab9be29a0dee9763aae9d396832d829cf
Alan Magliolini <alannm54@gmail.com>
2024-03-03 22:31:20 -0300

    actualizo el App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,7 +7,7 @@ import Home from "../components/common/home/Home";
 import History from "../components/pages/history/History";
 
 
-const App: React.FC = () => {
+const App = () => {
   const getUserData = useProductsAndPointsStore(state => state.getUserData)
   const loading  = useProductsAndPointsStore(state => state.loading)
   const getProducts = useProductsAndPointsStore(state => state.getProducts)
